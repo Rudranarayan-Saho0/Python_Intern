c9bc97eadfa3950756bf9b4a9134c3428aac3671
Rudra <rudraomm2005@gmail.com>
2024-08-02 19:53:31 +0530

    Create 5Divisible.py
    A Simple Python Program For Check Entered Number Divisible By 5 or Not.

---
+++ 5Divisible.py
@@ -0,0 +1,6 @@
+#Program For Check Entered NUmber Divisible By 5 or Not.
+number = int(input("Enter A Number For Check That Divisible By 5 or Not"))
+if number%5==0:
+    print(f'{number} is Divible By 5.')
+else:
+    print(f'{number} is Not Divisible By 5.')
