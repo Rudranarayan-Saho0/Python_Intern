714f4d9524dc350cac7865cf1071d8e496096495
Rudra <rudraomm2005@gmail.com>
2024-08-10 19:27:39 +0530

    Create Check_Angels.py
    Check An Inputed Triangles Angle are Acute, Obtuse, or Right Angle.

---
+++ Check_Angels.py
@@ -0,0 +1,14 @@
+#Check The Entered angle is Acute, Obtuse or Right Angle.
+
+Angle = float(input("Enter Angle In degrees: "))
+
+if 0<Angle<90:
+    print("The Angle Is Acute. ")
+elif Angle == 90:
+    print("The Angle Is RightAngle. ")
+elif 90<Angle<180:
+    print("The Angle Is Obtuse. ")
+else:
+    print("Invalid Angle")
+
+
