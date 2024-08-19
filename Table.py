0ecbfe0c74b812baebdc48e21785b6ab29133069
Rudra <rudraomm2005@gmail.com>
2024-08-19 09:31:14 +0530

    Create Table.py
    A Simple Python Program To Print The Table Of an Inputed Number.(While Loop)

---
+++ Table.py
@@ -0,0 +1,6 @@
+#A Simple Python program To Print The Table Of an Inputed Number.(While Loop)
+num = int(input("Enter A Number. "))
+i = 1
+while i<=10:
+    print(f"{num} x {i} = {num*i}")
+    i+=1
