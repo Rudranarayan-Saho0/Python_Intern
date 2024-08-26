dfe4a4dce8e0a4f66ed2dbdb5b0b52fe57767b03
Rudra <rudraomm2005@gmail.com>
2024-08-26 19:13:45 +0530

    Create OldestAmong3People.py
    Check The Age Of Three People And Determine The Oldest Among Them.

---
+++ OldestAmong3People.py
@@ -0,0 +1,7 @@
+#Check The Age Of Three People And Determine The Oldest Among Them.
+age1 = input("Enter Age Of Person 1: ")
+age2 = input("Enter Age Of Person 2: ")
+age3 = input("Enter Age Of Person 3: ")
+
+oldest = max(age1,age2,age3)
+print(f"The Oldest Among The Three Person Is: {oldest} Years Age Person.")
